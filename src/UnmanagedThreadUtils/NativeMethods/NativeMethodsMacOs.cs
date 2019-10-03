fb0d0534554d3d04225fce0b5a183e94347161d9
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-10-03 20:39:23 +0300

    code cleanup

--- src/UnmanagedThreadUtils/NativeMethods/NativeMethodsMacOs.cs
+++ src/UnmanagedThreadUtils/NativeMethods/NativeMethodsMacOs.cs
@@ -9,6 +9,12 @@ namespace UnmanagedThreadUtils.NativeMethods
     /// </summary>
     internal static class NativeMethodsMacOs
     {
+        /// <summary>
+        /// Create a thread-specific data key visible to all threads in the process.
+        /// </summary>
+        /// <param name="key">Key.</param>
+        /// <param name="destructorCallback">Destructor callback pointer.</param>
+        /// <returns>Zero when successful; error code otherwise.</returns>
         [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]        [DllImport("libSystem.dylib")]
         public static extern int pthread_key_create(IntPtr key, IntPtr destructorCallback);
