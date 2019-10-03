563f29a05be1b87cd53d91f875bcaa5c743e99ee
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-10-03 20:43:44 +0300

    code cleanup

--- src/UnmanagedThreadUtils/NativeMethods/NativeMethodsMacOs.cs
+++ src/UnmanagedThreadUtils/NativeMethods/NativeMethodsMacOs.cs
@@ -10,7 +10,7 @@ namespace UnmanagedThreadUtils.NativeMethods
     internal static class NativeMethodsMacOs
     {
         /// <summary>
-        /// Create a thread-specific data key visible to all threads in the process.
+        /// Creates a thread-specific data key visible to all threads in the process.
         /// </summary>
         /// <param name="key">Key.</param>
         /// <param name="destructorCallback">Destructor callback pointer.</param>
@@ -19,10 +19,21 @@ namespace UnmanagedThreadUtils.NativeMethods
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]        [DllImport("libSystem.dylib")]
         public static extern int pthread_key_create(IntPtr key, IntPtr destructorCallback);
 
+        /// <summary>
+        /// Deletes a thread-specific data key previously returned by <see cref="pthread_key_create"/>.
+        /// </summary>
+        /// <param name="key">Key to delete.</param>
+        /// <returns>Zero when successful; error code otherwise.</returns>
         [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]        [DllImport("libSystem.dylib")]
         public static extern int pthread_key_delete(int key);
 
+        /// <summary>
+        /// Associates a thread-specific value with a key obtained via a previous call to <see cref="pthread_key_create" />.
+        /// </summary>
+        /// <param name="key">Key to associate a value with.</param>
+        /// <param name="value">Value to associate with the key.</param>
+        /// <returns>Zero when successful; error code otherwise.</returns>
         [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass", Justification = "Reviewed.")]
         [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]        [DllImport("libSystem.dylib")]
         public static extern int pthread_setspecific(int key, IntPtr value);
